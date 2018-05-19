63e4dbd2bcbc4a01f4c5b692eeb1b3f467e10810
Blake Pastrana <blake.pastrana@accenturefederal.com>
2018-05-19 16:21:30 -0400

    GetExercise ID

--- Drive_API/src/main/java/drive/fitness/dao/ExerciseDao.java
+++ Drive_API/src/main/java/drive/fitness/dao/ExerciseDao.java
@@ -7,7 +7,9 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 import drive.fitness.models.Exercise;
+import drive.fitness.models.User;
 
 public interface ExerciseDao extends CrudRepository<Exercise, Long>{ 
-
+	@Query("FROM Exercise where userId=:userID")
+	public List<Exercise> getExerciseByUser(@Param("userID") int userId);
 }
