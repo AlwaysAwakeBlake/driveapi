dcac9e2cb0eefb522253ba495037ceeecfff1d00
Matthew Dromazos <dromazmj@dukes.jmu.edu>
2018-05-27 20:05:02 -0400

    Add classes to get files from an S3 Bucket * API function doesnt return proper data
    Signed-off-by: Matthew Dromazos <dromazmj@dukes.jmu.edu>

--- Drive_API/src/main/java/drive/fitness/controller/UserController.java
+++ Drive_API/src/main/java/drive/fitness/controller/UserController.java
@@ -1,9 +1,14 @@
 package drive.fitness.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+
 import drive.fitness.dao.*;
 import drive.fitness.models.*;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -18,8 +23,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import drive.fitness.s3.util.*;
+import drive.fitness.services.impl.S3Services;
+
+import drive.fitness.s3.util.Utility;
+
 @RestController
 public class UserController {
+	
+	@Autowired
+	private S3Services S3Services;
 
 	@Autowired
 	private UserDao userDao;
@@ -76,6 +89,12 @@ public class UserController {
     @RequestMapping(value = "/getUserId", method= RequestMethod.GET)
     public int getUserId(@RequestParam(value = "username", defaultValue = "test") String username) {
     	return userDao.getUserId(username);
+    }
+    
+    @RequestMapping(value = "/getUserProfilePic", method= RequestMethod.GET)
+    public String getUserProfilePic(@RequestParam(value = "username", defaultValue = "test") String username) throws IOException {
+    	Utility utility = new Utility();
+    	return utility.displayText(S3Services.downloadFile("profile_pics/" + username).getObjectContent());
     }
 
 }
